bf7274193624df093737501c880567ecf40b776e
margherita1234 <busato.margherita@gmail.com>
2024-09-02 21:05:08 +0200

    modifica problema con dist

--- components.d.ts
+++ components.d.ts
@@ -10,15 +10,12 @@ declare module 'vue' {
     __test__: typeof import('./src/components/__test__.vue')['default']
     Accesso: typeof import('./src/components/Accesso.vue')['default']
     Artista: typeof import('./src/components/artista.vue')['default']
-    Benvenuto: typeof import('./src/components/Benvenuto.vue')['default']
     Home: typeof import('./src/components/home.vue')['default']
     Modifyprofile: typeof import('./src/components/modifyprofile.vue')['default']
     Opera: typeof import('./src/components/opera.vue')['default']
     Profilo: typeof import('./src/components/profilo.vue')['default']
-    Prova: typeof import('./src/components/prova.vue')['default']
     Ricerca: typeof import('./src/components/ricerca.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    Test__: typeof import('./src/components/test__.vue')['default']
   }
 }
